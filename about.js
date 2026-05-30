383517767a05a2cbde09299d812dac396d07eda4
17byseventeen <grace.totten11@gmail.com>
2026-05-30 17:37:59 -0300

    Add files via upload

--- about.js
+++ about.js
@@ -11,8 +11,21 @@ const teamData = [
         }
     },
     {
+        name: "Leah Sherwood",
+        role: "Editor and Social Media Manager for 17 By Seventeen",
+        bio: "Hi! My name is Leah Sherwood and I am a grade 10 student in Saint John, New Brunswick. Some of my passions include the environment, the arts, public speaking, and working with others, both to learn and to work toward a common goal. I am a grade representative on my student council, a member of the UNESCO club at my school, a member of my school's choir and drama clubs, have co-taught a biomaking program, and I am working on launching a project of my own relating to sustainability and environmental education and action. I love helping and making an impact on people in my community, and I often find myself volunteering and taking part in charitable events and action projects in my community. I am honoured to be a part of the 17 By Seventeen challenge, and am so excited to see where it takes us.",
+        photo: "images/leah-photo.jpg",
+        social: {
+            // Add social media links if available
+        }
+    }
+];
+
+// Former team member data
+const formerTeamData = [
+    {
         name: "Willow McGrath",
-        role: "New Brunswick Representative for 17 By Seventeen",
+        role: "Former New Brunswick Representative for 17 By Seventeen",
         bio: "My name is Willow McGrath and I'm a grade 12 student in Saint John NB. I am extremely passionate about the environment and love working with people. I am a camp counsellor and spend all of my time outside in the summer, while providing experiential learning to campers and more. I also volunteer for a local organization that works to protect the biodiversity in my city. I lead the UNESCO club at my school, which has given my school a green space and helped get feminine hygiene products in every bathroom, along with many other projects last year. This year we'll create more greenspaces, provide outdoor education and highlight other SDGs through projects such as a sexual assault support day and awareness campaigns about AI. I've represented Canadian youth on matters of environmental education, human rights and the SDGs at many conferences including the National Forum for Environmental Learning, AP Nature Ed/Black Diaspra conferences and CCUNESCO conferences. I'm also extremely grateful to have had the opportunity to represent the younger generation at the IUCNs World Conservation Congress this past October.",
         photo: "images/willow-photo.jpg",
         social: {
@@ -21,7 +34,7 @@ const teamData = [
     },
     {
         name: "Anjani Shah",
-        role: "Ontario Representative for 17 By Seventeen",
+        role: "Former Ontario Representative for 17 By Seventeen",
         bio: "Hi! My name is Anjani Shah, and I am a 16 year old student at Assumption College School who's passionate about making a difference in my school, community, and the world. I am a strong believer that real change starts with small actions, and I try my best to live that every day through leadership, volunteering, and service. Some things I am a part of at school are Model UN, Mock Trial, and STEM, where I get to challenge myself and grow as a leader and problem-solver. I am also part of the 180 Team, which focuses on helping our community by taking part in projects that support people through local services such as therapy centres and soup kitchens. I am also a Prefect; where I help organize school events and tutor students who need academic support. Now, a sneak peak of my life outside of school. My culture and religion are a huge part of me in which through my temple's charity (BAPS Charities), I help raise funds for cancer research and breast cancer awareness as well as supporting local food bank initiatives. I also teach young girls at my temple every Saturday about my culture and faith, and through Army Cadets, I have raised money to support Canadian Armed Forces members, Veterans, and their families. Being part of the 17bySeventeen movement motivates me to take action toward the United Nations Sustainable Development Goals (UNSDGs) and remind others that you're never too young to make a difference. Together, we can help build a kinder, more sustainable world, one step at a time.",
         photo: "images/anjani-photo.jpg",
         social: {
@@ -30,7 +43,7 @@ const teamData = [
     },
     {
         name: "Olivia Cheng",
-        role: "Ontario Representative for 17 By Seventeen",
+        role: "Former Ontario Representative for 17 By Seventeen",
         bio: "My name is Olivia Cheng and I'm a grade 11 student in Ontario. I'm passionate about the arts, environmental stewardship, and building connections in my community. I run a youth-led art nonprofit that provides free art lessons to underprivileged students in Ontario because I believe creativity and a welcoming space can inspire people to view the world with possibility. I've represented Canadian and global youth on the topic of nature-based education at the National Framework for Environmental Learning as well as the IUCN World Conservation Congress. From being a part of my school's sustainability council to organizing the annual student-body art show, I am motivated to highlight the power of youth engagement. As the Ontario representative for 17 By Seventeen, I'm excited to use my strengths to grow a network of Canadian youth dedicated to completing the UN SDGs.",
         photo: "images/olivia-photo.jpg",
         social: {
@@ -38,17 +51,8 @@ const teamData = [
         }
     },
     {
-        name: "Leah Sherwood",
-        role: "Editor and Social Media Manager for 17 By Seventeen",
-        bio: "Hi! My name is Leah Sherwood and I am a grade 10 student in Saint John, New Brunswick. Some of my passions include the environment, the arts, public speaking, and working with others, both to learn and to work toward a common goal. I am a grade representative on my student council, a member of the UNESCO club at my school, a member of my school's choir and drama clubs, have co-taught a biomaking program, and I am working on launching a project of my own relating to sustainability and environmental education and action. I love helping and making an impact on people in my community, and I often find myself volunteering and taking part in charitable events and action projects in my community. I am honoured to be a part of the 17 By Seventeen challenge, and am so excited to see where it takes us.",
-        photo: "images/leah-photo.jpg",
-        social: {
-            // Add social media links if available
-        }
-    },
-    {
         name: "Carter Mochinski",
-        role: "British Columbia Representative for 17 By Seventeen",
+        role: "Former British Columbia Representative for 17 By Seventeen",
         bio: "Hello! My name is Carter Mochinski, and I am from Lake Country, BC. I'm 17 years old in grade 12 at George Elliot Secondary School. I am so excited to join 17bySeventeen and make a difference in our communities. My passion for making a difference is motivated most by SDG 10, Reducing Inequalities and 13 Climate Action. I dream of a Canada and a world where all are valued and appreciated, and as a world that values protecting our environment and preserving our beautiful land. I call on the government to put talk into action and strive to achieve the UN SDGs by 2030. I am serving on multiple councils and working with non-profit organizations where I am advocating for the goals. I am currently serving a term on Lake Country Council as a Youth Councillor and on the SD23 District Student Council. I am going to COP30 in Bélem, Brazil, with CGE as the Global Head Reporter. I will also be on a couple of panels at COP30 representing BC Parks Foundation and CGE. I can't wait to take action with 17bySeventeen as well as educate our communities on the 17 Sustainable Development Goals. 17bySeventeen brings together youth with powerful connections, clearly identifying an important goal to achieve all the goals before it, SDG 17, Partnership for the Goals, on a national level. Because if we don't work together as a society, how are we going to achieve our global goals?",
         photo: "images/carter-photo.jpg",
         social: {
@@ -57,7 +61,7 @@ const teamData = [
     },
     {
         name: "Madhu Deiva Sandhiya",
-        role: "Nova Scotia Representative for 17 By Seventeen",
+        role: "Former Nova Scotia Representative for 17 By Seventeen",
         bio: "Hello! I'm Madhu D.S and I am a Grade 10 at Woodlawn High School from Dartmouth, Nova Scotia. I am a person who is always seeking self/surrounding evolving opportunities. I have a deep passion for making an impact in the community so that people can lead better lives, feel empowered to create change, have equal opportunities to thrive and live in a more sustainable environment. I work with United for Literacy as a tutor to teach kids whose first languages aren't English in a safe and understanding environment. I am a teen ambassador for the Canadian Association for Girls in Science. In school, I am active in clubs and councils that support and give back to the school and community. To wrap it all up, I am a strong believer of leading with integrity, equality, empowering others and taking action to create lasting change. I am looking forward to play a part in 17 by Seventeen!",
         photo: "images/madhu-photo.png.JPG",
         social: {
@@ -66,7 +70,7 @@ const teamData = [
     },
     {
         name: "María José Amor",
-        role: "Argentina Representative for 17 By Seventeen",
+        role: "Former Argentina Representative for 17 By Seventeen",
         bio: "¡Hola! My name is María José Amor, I'm 18 years old and I come from Buenos Aires, Argentina. As the newest member, I'm committed to creating social and environmental impact in my country, but also proud to represent Argentina as part of this project. I've always tried to be involved in community and sustainability-focused projects, including planning a native plant nursery, creating and participating in local volunteering opportunities, and, more recently, contributing to youth advocacy at the World Conservation Congress. Growing up in an area affected by events like wildfires, water pollution, and species loss has helped to fuel my interest in sustainable development and reinforced my belief that the SDGs must be integrated into every aspect of my life: as an individual, within my institutions, and as an active citizen. Joining this project is an exciting opportunity to collaborate with inspiring people, contribute my perspective, and help advance meaningful and hopefully long-term impact while representing my country and learning from others.",
         photo: "images/maria-photo.png",
         social: {
@@ -74,11 +78,7 @@ const teamData = [
             // instagram: "https://instagram.com/username",
             // email: "email@example.com"
         }
-    }
-];
-
-// Former team member data
-const formerTeamData = [
+    },
     {
         name: "Mateo Porter Partida",
         role: "Former British Columbia Representative for 17 By Seventeen",
@@ -88,17 +88,6 @@ const formerTeamData = [
             // Add social media links if available
         }
     }
-    // Add more former team members here following this format:
-    // {
-    //     name: "Team Member Name",
-    //     role: "Team Member Role",
-    //     bio: "Brief bio about the team member",
-    //     photo: "images/team-member-photo.jpg", // Optional - leave empty or null for placeholder
-    //     social: {
-    //         instagram: "https://instagram.com/username", // Optional
-    //         email: "email@example.com" // Optional
-    //     }
-    // }
 ];
 
 // Create team member card
